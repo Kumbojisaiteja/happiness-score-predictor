d9db562af7a78af8e7b3a65d6ef81f27a3c44250
Kumbojisaiteja <kumbojisaiteja@gmail.com>
2026-03-11 13:19:49 +0530

    fixed flask code

--- app.py
+++ app.py
@@ -1,39 +1,9 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
-
-model = pickle.load(open("bestmodel.pkl","rb"))
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/prediction")
-def prediction():
-    return render_template("prediction.html")
-
-@app.route("/predict", methods=["POST"])
-def predict():
-
-    economy=float(request.form["economy"])
-    family=float(request.form["family"])
-    health=float(request.form["health"])
-    freedom=float(request.form["freedom"])
-    trust=float(request.form["trust"])
-    generosity=float(request.form["generosity"])
-
-    features=np.array([[economy,family,health,freedom,trust,generosity]])
-
-    result=model.predict(features)
-
-    return render_template("result.html",prediction=round(result[0],3))
-from flask import Flask, render_template, request
-import numpy as np
-import pickle
 import os
 
+# Load model
 model = pickle.load(open("bestmodel.pkl","rb"))
 
 app = Flask(__name__)
